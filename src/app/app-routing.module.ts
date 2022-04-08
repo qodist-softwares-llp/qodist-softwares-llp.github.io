5978084ade1ca0ec8be0041201abcad30faa53c4
Dharmik Parmar <dharmikparmar0474@gmail.com>
2022-04-08 22:00:41 +0530

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+
+const routes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'services',
+    component: ServicesComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
